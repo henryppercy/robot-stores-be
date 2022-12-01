2d02074f899fb4ee8b78e1361e1407d62a33ae22
Henry <henryppercy@gmail.com>
2022-12-01 13:27:20 +0000

    Built controller class to deal with the two routes

---
+++ src/controllers/ProductsController.ts
@@ -0,0 +1,18 @@
+import Express from 'express';
+import productsService from '../services/productsService';
+
+class ProductsController {
+    getProducts = (req: Express.Request, res: Express.Response) => {
+        productsService.getProducts()
+            .then((products) => res.json(products));
+    }
+
+    getProduct = (req: Express.Request, res: Express.Response) => {
+        console.log('Products controller: getProduct');
+        let id = req.params.id;
+        productsService.getProduct(id)
+            .then((product) => res.json(product));
+    }
+}
+
+export const productsController = new ProductsController;
