f694b436f93b9361d1dcd1ca0d07d4c6ba6c4bf3
Henry <henryppercy@gmail.com>
2022-12-01 14:08:08 +0000

    Updated transpilled JavaScript

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = require("./config/routes");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = 3001;
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 (0, routes_1.routes)(app);
 app.listen(port, () => {
