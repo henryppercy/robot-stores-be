def7a8acb1783397070bba30ed0fd9703d591563
Henry <henryppercy@gmail.com>
2022-12-01 13:58:51 +0000

    Removed unnecessary type hinting

--- src/services/DbService.ts
+++ src/services/DbService.ts
@@ -1,11 +1,9 @@
 import * as mongoDB from "mongodb";
 
-mongoDB
-
 class DbService {
-    url: string = 'mongodb://root:password@localhost:27017';
+    url = 'mongodb://root:password@localhost:27017';
     client: mongoDB.MongoClient = new mongoDB.MongoClient(this.url);
-    dbName: string = 'robot_stores';
+    dbName = 'robot_stores';
 
     connectToDB = async (): Promise<mongoDB.Db> => {
         await this.client.connect();
