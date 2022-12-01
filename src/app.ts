d4ef3c57b7945418a545b0e1bc571605b79aa02d
Henry <henryppercy@gmail.com>
2022-12-01 14:05:22 +0000

    Included cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import Express from 'express';
 import { routes } from './config/routes';
+import cors from 'cors';
 
 const app = Express();
 const port = 3001;
 
+app.use(cors());
 app.use(Express.json());
+
 routes(app);
 
 app.listen(port, () => {
